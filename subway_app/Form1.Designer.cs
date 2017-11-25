b0890c8807c89349de78bf28d7f098293aa01c6e
jay51 <nenojaafar3@gmail.com>
2017-11-25 12:39:36 -0600

    added the entire program to the repo

---
+++ subway_app/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace subway_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.steak = new System.Windows.Forms.Button();
+            this.tuna = new System.Windows.Forms.Button();
+            this.lblone = new System.Windows.Forms.Label();
+            this.info = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // steak
+            // 
+            this.steak.Location = new System.Drawing.Point(328, 93);
+            this.steak.Name = "steak";
+            this.steak.Size = new System.Drawing.Size(75, 23);
+            this.steak.TabIndex = 0;
+            this.steak.Text = "STEAK";
+            this.steak.UseVisualStyleBackColor = true;
+            this.steak.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tuna
+            // 
+            this.tuna.Location = new System.Drawing.Point(328, 122);
+            this.tuna.Name = "tuna";
+            this.tuna.Size = new System.Drawing.Size(75, 23);
+            this.tuna.TabIndex = 1;
+            this.tuna.Text = "TUNA";
+            this.tuna.UseVisualStyleBackColor = true;
+            this.tuna.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblone
+            // 
+            this.lblone.AutoSize = true;
+            this.lblone.Location = new System.Drawing.Point(42, 103);
+            this.lblone.Name = "lblone";
+            this.lblone.Size = new System.Drawing.Size(40, 13);
+            this.lblone.TabIndex = 2;
+            this.lblone.Text = "LABEL";
+            // 
+            // info
+            // 
+            this.info.Location = new System.Drawing.Point(243, 253);
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(75, 23);
+            this.info.TabIndex = 3;
+            this.info.Text = "message";
+            this.info.UseVisualStyleBackColor = true;
+            this.info.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 326);
+            this.Controls.Add(this.info);
+            this.Controls.Add(this.lblone);
+            this.Controls.Add(this.tuna);
+            this.Controls.Add(this.steak);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button steak;
+        private System.Windows.Forms.Button tuna;
+        private System.Windows.Forms.Label lblone;
+        private System.Windows.Forms.Button info;
+    }
+}
+
---
+++ subway_app/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace subway_app
+{
+    public partial class Form1 : Form
+    {
+        public Label[] lbllist;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button btnName = (Button)sender;
+            if (btnName == tuna || btnName == steak)
+            {
+                lblone.Text = btnName.Text;
+            }
+            //this to show a message box.
+            else if (btnName == info)
+            {
+                MessageBox.Show("this futuare is not avialble yet, comming soon!");
+            }
+            else
+            {
+                lbllist = new Label[5];
+                lbllist[] = "hello world";
+            }
+            
+        }
+        // this is to show the box befor closing the form
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result;
+            result = MessageBox.Show("Are you sure you want to close the applciation", "now", MessageBoxButtons.YesNo);
+            if (result == DialogResult.No)
+            {
+                //if the user click no then we cancel the closing.
+                e.Cancel = true;
+            }
+
+        }
+
+
+
+
+
+    }
+}
